c6fb3fa9bbcc4c3e04169bc7f8a46b5853b1f27a
huybqhgkkk <huybqhg@gmail.com>
2023-03-01 23:24:49 +0700

    ud2

--- src/component/PushNotificationButton.js
+++ src/component/PushNotificationButton.js
@@ -9,17 +9,17 @@ function PushNotificationButton() {
                     applicationServerKey: 'BLR6jMQ3R5NlQcQzXc0vFBtArPl4iFfC9kq0O34AZXGMCgEKfI2lKsKNWEcnKN8c3QfW6ba3wHttTk6byecz-6w'
                 }).then(function(subscription) {
                     console.log('Push notification subscription successful: ', subscription);
-                    fetch('/subscribe', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(subscription)
-                    }).then(function(response) {
-                        console.log('Push notification subscription saved on server.');
-                    }).catch(function(error) {
-                        console.error('Push notification subscription failed: ', error);
-                    });
+                    // fetch('/subscribe', {
+                    //     method: 'POST',
+                    //     headers: {
+                    //         'Content-Type': 'application/json'
+                    //     },
+                    //     body: JSON.stringify(subscription)
+                    // }).then(function(response) {
+                    //     console.log('Push notification subscription saved on server.');
+                    // }).catch(function(error) {
+                    //     console.error('Push notification subscription failed: ', error);
+                    // });
                 });
             });
         }
